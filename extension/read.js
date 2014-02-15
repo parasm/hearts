a625204abd497093a333d678002f976f778a37ea
DiZy <davzil@bergen.org>
2014-02-15 15:46:22 -0500

    readjs

--- extension/read.js
+++ extension/read.js
@@ -1,4 +1,20 @@
 document.body.style.backgroundColor="red";
+
+
+var chats = $(document.getElementsByClassName("titlebarTextWrapper")); //change name
+var chat = chats[0];
+
+var viewportOffset = chat.getBoundingClientRect();
+
+var offsetLeft = viewportOffset.left + ((window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft);
+var offsetTop = viewportOffset.top + ((window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop);
+var btn = document.createElement("BUTTON")
+var t = document.createTextNode("Test Button");
+btn.appendChild(t);
+btn.offsetTop = offsetTop;
+btn.offsetLeft = offsetLeft;
+document.head.appendChild(btn);
+
 $('document').ready(function(){
     console.log("start");
     try{
