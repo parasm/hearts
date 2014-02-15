f42485c648eeca9ef7db1b83b832b60a5758d6bb
Paras Modi <parasm346@gmail.com>
2014-02-14 20:44:19 -0500

    added index.html

--- server/app.py
+++ server/app.py
@@ -1,1 +1,16 @@
-import Flask
+import os
+import re
+from pymongo import MongoClient
+from flask import Flask, render_template, request, session, redirect, escape
+import jinja2
+
+app = Flask(__name__)
+app.secret_key = 'paras_is_the_slim_reaper'
+
+@app.route('/')
+def hello():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 8000))
+	app.run(host='0.0.0.0', port=port,debug=True)
