6053298cacbadecb162a655f105256d8c85b5c05
Paras Modi <parasm346@gmail.com>
2014-02-15 02:46:49 -0500

    errors

--- app.py
+++ app.py
@@ -14,14 +14,19 @@ def hello():
 @app.route('/hearts')
 def love():	
 	counter = []
+	count_dict = {}
 	user = facebook.get_user_from_cookie(request.cookies,'1441116782789661','608a6502fb85bbbe7e0cafabcaa8832e')
 	token = user.get('access_token')
 	graph = facebook.GraphAPI(token)
 	profile = graph.get_object("me")
+	me = profile.get('first_name') +" "+ profile.get('last_name')
 	inbox = graph.get_connections("me","inbox")
 	data = inbox.get('data')
 	for w in data:
-		messages = w.get('comments').get('data')
+		try:
+			messages = w.get('comments').get('data')
+		except AttributeError, e:
+			break
 		name1 = [None,0]
 		name2 = [None,0]
 		for x in messages:
@@ -39,8 +44,11 @@ def love():
 				name2[1] +=1
 		print name1[0] + " count: " + str(name1[1])
 		counter.append(str(name1[0]) + " count: " + str(name1[1]))
+		count_dict[name1[0]] = name1[1]
 		print name2[0] + " count: " + str(name2[1])
 		counter.append(str(name2[0]) + " count: " + str(name2[1]))
+		count_dict[name2[0]] = name2[1]
+	print count_dict
 	return render_template('hearts.html', counter=counter)
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
