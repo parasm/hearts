d11acf9b93b6b4cbb8078b03174e414995fee34e
DiZy <davzil@bergen.org>
2014-02-15 03:31:33 -0500

    idk javascript

--- extension/baradder.js
+++ extension/baradder.js
@@ -1,6 +1,15 @@
 var btn = document.createElement("BUTTON")
-    var t = document.createTextNode("CLICK ME");
-    btn.appendChild(t);
-    document.body.appendChild(btn);
-    var chat = $(document.getElementById("Paras Modi")); //change name
-    var top = parseInt(chat.style.top.split('px')[0]);
+var t = document.createTextNode("CLICK ME");
+btn.appendChild(t);
+document.body.appendChild(btn);
+
+
+var chat = $(document.getElementById("Paras Modi")); //change name
+var top = parseInt(chat.style.top.split('px')[0]);
+var  = parseInt(chat.style.top.split('px')[0]);
+var newdiv = document.createElement("div");
+newdiv.id = "HeartsMonitor";
+newdiv.style.top = top;
+
+
+document.body.appendChild(newdiv);
