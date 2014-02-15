623e7c8b454fc900e2f8a8db5f5ca09e27dd3718
Paras Modi <parasm346@gmail.com>
2014-02-15 10:22:01 -0500

    annoying chrome

--- extension/read.js
+++ extension/read.js
@@ -1,21 +1,21 @@
 $('document').ready(function(){
+    console.log("start");
     try{
-	
-		console.log("try");
+        console.log("try");
         chrome.cookies.get({ url: 'http://localhost:8000', name: 'id_code' },
         function (cookie) {
             if (cookie) {
-				console.log("found cookie");
+              console.log("found cookie");
               doRequest(cookie.value);
             }
             else{
                 $('body').append("<h2>No id cookie found. Go to <a href='http://hearts3.herokuapp.com'>http://hearts3.herokuapp.com</a> to generate one</h2>");
-				console.log("No cookie found");
+                console.log("No cookie found");
             }
         });
     }catch(err){
-		console.log("Error getting id code from localhost");
-		console.log(err);
+        console.log("Error getting id code from localhost");
+        console.log(err);
     }
     function doRequest(cookie){
         $.ajax({
