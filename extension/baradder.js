9718eff5e184e256772a97ab08062df7b84a1278
DiZy <davzil@bergen.org>
2014-02-15 09:04:25 -0500

    baradder bs

--- extension/baradder.js
+++ extension/baradder.js
@@ -1,15 +1,14 @@
+
+var chats = $(document.getElementsByClassName("titlebarTextWrapper")); //change name
+var chat = chats[0];
+
+var viewportOffset = chat.getBoundingClientRect();
+
+var offsetLeft = viewportOffset.left + ((window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft);
+var offsetTop = viewportOffset.top + ((window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop);
 var btn = document.createElement("BUTTON")
-var t = document.createTextNode("CLICK ME");
+var t = document.createTextNode("Test Button");
 btn.appendChild(t);
+btn.offsetTop = offsetTop;
+btn.offsetLeft = offsetLeft;
 document.body.appendChild(btn);
-
-
-var chat = $(document.getElementById("Paras Modi")); //change name
-var top = parseInt(chat.style.top.split('px')[0]);
-var  = parseInt(chat.style.top.split('px')[0]);
-var newdiv = document.createElement("div");
-newdiv.id = "HeartsMonitor";
-newdiv.style.top = top;
-
-
-document.body.appendChild(newdiv);
