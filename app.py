b1d472277621632b1904eacd0264f9486c7e38c7
Paras Modi <parasm346@gmail.com>
2014-02-15 01:37:35 -0500

    counting and shit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import re
 import facebook
 import requests
-from pymongo import MongoClient
 from flask import Flask, render_template, request, session, redirect, escape
 import jinja2
 
@@ -13,16 +12,28 @@ app.secret_key = 'paras_is_the_slim_reaper'
 def hello():
 	return render_template('index.html')
 @app.route('/hearts')
-def love():
-	#token = request.cookies.get('fbsr_1441116782789661')
+def love():	
 	user = facebook.get_user_from_cookie(request.cookies,'1441116782789661','608a6502fb85bbbe7e0cafabcaa8832e')
 	token = user.get('access_token')
 	graph = facebook.GraphAPI(token)
 	profile = graph.get_object("me")
-	#friends = graph.get_connections("me", "friends")
-	#print friends
 	inbox = graph.get_connections("me","inbox")
-	print inbox.get('data')[0].get('comments').get('data')
+	name1 = [None,0]
+	name2 = [None,0]
+	messages = inbox.get('data')[0].get('comments').get('data')
+	for x in messages:
+		name = x.get('from').get('name')
+		if not(name1[0]):
+			name1[0] = name
+		else:
+			name1[1]+=1
+		if not(name2[0]) or name1[0] != name:
+			name2[0] = name
+		else:
+			name2[1]+=1
+		print name
+	print name1[0] + " count: " + str(name1[1])
+	print name2[0] + " count: " + str(name2[1])
 	return render_template('hearts.html')
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
