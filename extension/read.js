f0a3c1fa9a6134dc16367cfc4da3d4ddac028dd8
Kai Kuehner <kvk@pobox.com>
2014-02-15 09:26:18 -0500

    Added facebook extension debug info

--- extension/read.js
+++ extension/read.js
@@ -1,16 +1,21 @@
 $('document').ready(function(){
     try{
+	
+		console.log("try");
         chrome.cookies.get({ url: 'http://localhost:8000', name: 'id_code' },
         function (cookie) {
             if (cookie) {
+				console.log("found cookie");
               doRequest(cookie.value);
             }
             else{
                 $('body').append("<h2>No id cookie found. Go to <a href='http://hearts3.herokuapp.com'>http://hearts3.herokuapp.com</a> to generate one</h2>");
+				console.log("No cookie found");
             }
         });
     }catch(err){
-        //do nothing
+		console.log("Error getting id code from localhost");
+		console.log(err);
     }
     function doRequest(cookie){
         $.ajax({
